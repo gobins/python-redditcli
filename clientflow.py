c94d7bff8dca7fef8d35e8e7c82477df45d073bf
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2015-06-23 20:08:15 +1000

    Added get_token

--- clientflow.py
+++ clientflow.py
@@ -1,1 +1,36 @@
-from oauth2client.client import flow_from_clientsecrets
+import requests
+import requests.auth
+import ConfigParser
+
+
+def get_auth_token():
+    config = ConfigParser.ConfigParser()
+    config.read('client_secrets.ini')
+    client_auth = requests.auth.HTTPBasicAuth(
+        config.get('reddit_bot', 'client_id'),
+        config.get('reddit_bot', 'client_secret')
+    )
+    post_data = {
+        "grant_type": "password",
+        "username": config.get('reddit_bot', 'user'),
+        "password": config.get('reddit_bot', 'password')
+    }
+    headers = {
+        "User-Agent": "python-app/0.1 by "+config.get('reddit_bot', 'user')
+    }
+
+    response = requests.post(
+        "https://www.reddit.com/api/v1/access_token",
+        auth=client_auth,
+        data=post_data,
+        headers=headers
+    )
+    print response.json()
+
+
+def main():
+    get_auth_token()
+
+
+if __name__ == "__main__":
+    main()
